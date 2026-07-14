044207e7be33fc9c9ab1c5841b4d2ae4a30123d7
Sviat6239 <sviatoslav6239@gmail.com>
2026-07-14 12:55:52 +0200

    Add data structures and utility functions for scene management

--- sor.c
+++ sor.c
@@ -1,4 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+typedef struct
+{
+    const char *text;
+    int next_scene;
+    int next_line;
+} Choice;
+
+typedef struct
+{
+    const char persona_name;
+    bool is_persona;
+    const char *text;
+    int next_scene;
+    int next_line;
+} Line;
+
+typedef struct
+{
+    const char *description;
+    int i;
+    Choice choice[i];
+    int choice_count;
+} Scene;
+
+void clear_screen()
+{
+    printf("\033[H\033[J");
+    fflush(stdout);
+}
+
+void type_text(const char *text, unsigned int delay_us)
+{
+    while (*text)
+    {
+        putchar(*text++);
+        fflush(stdout);
+        usleep(delay_us);
+    }
+    printf("\n");
+}
 
 int main()
 {
